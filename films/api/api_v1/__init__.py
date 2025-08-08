c913598b40f99b2b5249013fe842d20a36d98e23
BOSOZOKU <matveygrishayev@mail.ru>
2025-08-08 14:09:01 +0300

    added routers

--- films/api/api_v1/__init__.py
+++ films/api/api_v1/__init__.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from .films.views import router as views_films_router
+from .film.views import router as views_films_router
 
 router = APIRouter(prefix="/v1")
 router.include_router(router=views_films_router)
