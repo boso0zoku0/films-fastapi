b95309f0dbd039ac9082c14ada665dc1e390d040
BOSOZOKU <matveygrishayev@mail.ru>
2025-10-08 19:46:17 +0300

    change main_views(root_view)

--- films/api/main_views.py
+++ films/api/main_views.py
@@ -30,10 +30,9 @@ def about_page(request: Request) -> HTMLResponse:
     )
 
 
-@router.get("/")
-def read_root(request: Request, name: str = "World") -> dict[str, str]:
-    docs_url = request.url.replace(path="/docs", query="")
-    return {
-        "message": f"Hello {name}",
-        "docs": str(docs_url),
-    }
+@router.get("/", name="root")
+def read_root(request: Request) -> HTMLResponse:
+    return templates.TemplateResponse(
+        request=request,
+        name="base.html",
+    )
